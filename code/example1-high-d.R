1e4045c7e41c6ce1914cb85224a0771f3e3d4843
huizezhang-sherry <huizezhangsh@gmail.com>
2024-06-10 12:56:56 -0500

    edit application 1

--- code/example1-high-d.R
+++ code/example1-high-d.R
@@ -18,12 +18,24 @@ historyarray2dt <- function(array, index_f){
 
 }
 
-sim <- function(d, optimiser = c("search_better", "search_jellyfish"), data_seed = 123456, optim_seed = seed){
+sim <- function(d, optimiser = c("search_better_random", "search_better", "search_jellyfish"),
+                data_seed = 123456, optim_seed = seed){
   set.seed(data_seed)
   pipe1000 <- pipeData(d, 1000) %>% scale() %>% as_tibble()
   colnames(pipe1000) <- paste0("V", 1:d)
 
-  if (optimiser == "search_better"){
+  if (optimiser == "search_better_random"){
+    res <- map_dfr(optim_seed, function(seed){
+      set.seed(seed)
+      spiral_better <- save_history(
+        pipe1000, guided_tour(
+          holes(), search_f = search_better_random, max.tries = 1000
+        )
+      )
+      historyarray2dt(spiral_better[,,-1], holes())
+    }, .id = "sim") |>
+      mutate(d = d)
+  } else if (optimiser == "search_better"){
     res <- map_dfr(optim_seed, function(seed){
       set.seed(seed)
       spiral_better <- save_history(
@@ -34,20 +46,7 @@ sim <- function(d, optimiser = c("search_better", "search_jellyfish"), data_seed
       historyarray2dt(spiral_better[,,-1], holes())
     }, .id = "sim") |>
       mutate(d = d)
-  } else if (optimiser == "search_jellyfish"){
-
-    res <- map_dfr(optim_seed, function(seed){
-      set.seed(seed)
-        animate_xy(pipe1000, guided_tour(
-          holes(), d = 2, n_jellies = 1000,
-          search_f =  search_jellyfish,
-          max.tries = 300, min.tries = 100
-        ), max_frames = 50)
-    }, .id = "sim") |>
-      mutate(d = d)
-
   }
-
   return(res)
 }
 
@@ -56,32 +55,34 @@ data <- map_dfr(c(6, 8, 10, 12), ~{
   tibble(x = list(pipeData(.x, 1000) %>% scale()))
 }) |>
   mutate(dim = c(6, 8, 10, 12))
-safe_sim <- safely(sim)
-#a <- map_dfr(c(6, 8, 10, 12), ~safe_sim(d = .x, optimiser = "search_better", optim_seed = seed), .id = "dim")
+# a <- map_dfr(c(6, 8, 10, 12),
+#              ~safe_sim(d = .x, optimiser = "search_better",
+#                        optim_seed = seed),
+#              .id = "dim"
+#              )
 # take around an hour to run
-b <- map_dfr(
-  c(6, 8, 10, 12),
-  ~safe_sim(d = .x, optimiser = "search_jellyfish", optim_seed = seed),
-  .id = "dim")
+c <- map_dfr(c(6, 8, 10, 12),
+             ~sim(d = .x, optimiser = "search_better_random",
+                       optim_seed = seed),
+             .id = "dim"
+             )
 
-#save(b, file = "data/pipe_raw.rda")
-# pipe_better <- a |>
-#   unnest(result) |>
-#   group_by(sim, dim) |>
-#   filter(index_val == max(index_val), row_number() == max(row_number())) |>
-#   left_join(data, c("d" = "dim")) |>
-#   mutate(proj = list(as_tibble(x[[1]] %*% basis[[1]])), optimiser = "better") |>
-#   dplyr::select(-x, -id, -d) |>
-#   ungroup()
+
+pipe_better <- a |>
+  group_by(sim, dim) |>
+  filter(index_val == max(index_val), row_number() == max(row_number())) |>
+  left_join(data, c("d" = "dim")) |>
+  mutate(proj = list(as_tibble(x[[1]] %*% basis[[1]])), optimiser = "better") |>
+  dplyr::select(-x, -id, -d) |>
+  ungroup()
+
+pipe_better_random <- c |>
+  group_by(sim, dim) |>
+  filter(index_val == max(index_val)) |>
+  filter(row_number() == max(row_number())) |>
+  left_join(data, c("d" = "dim")) |>
+  mutate(proj = list(as_tibble(x[[1]] %*% basis[[1]])), optimiser = "better_random") |>
+  dplyr::select(-x, -id, -d) |>
+  ungroup()
 # save(pipe_better, file = "data/pipe_better.rda")
-# pipe_jellyfish <- b |>
-#   unnest(result) |>
-#   group_by(sim, dim) |>
-#   filter(index_val == max(index_val)) |>
-#   left_join(data, c("d" = "dim")) |>
-#   mutate(proj = list(as_tibble(x[[1]] %*% basis[[1]])), optimiser = "jellyfish") |>
-#   dplyr::select(dim, sim, basis, index_val, proj, optimiser) |>
-#   ungroup()
-# save(pipe_jellyfish, file = "data/pipe_jellyfish.rda")
-#
-#
+# save(pipe_better_random, file = here::here("data/pipe_better_random.rda"))
